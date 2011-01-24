2bd9f93d0c89918d9a8bb3edb73c0749d35075dd
Scott <scott@cloudspace.com>
2011-01-24 13:28:43 -0500

    fixing some of the reports

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -3,9 +3,15 @@ class Admin::ReportsController < Admin::ApplicationController
     num_users = User.count()
     #once again....not sure how to do the following in active record
     #select *,(7 - (select count(*) from courses_users where know = '1' and courses_users.course_id = courses.id)) dont_know from courses order by dont_know desc    
-    sub1 = "(#{num_users} - (select count(*) from courses_users where courses_users.know = '1' and courses_users.course_id = courses.id)) dont_know"
-    sub2 = "(select name from levels where levels.id = courses.level_id) level_name"
-    @courses = ActiveRecord::Base.connection.execute "select name,#{sub1},#{sub2} from courses order by level_id, dont_know DESC"
+    # sub1 = "(#{num_users} - (select count(*) from courses_users where courses_users.know = '1' and courses_users.course_id = courses.id)) dont_know"
+    #     sub2 = "(select name from levels where levels.id = courses.level_id) level_name"
+    #     @courses = ActiveRecord::Base.connection.execute "select name,#{sub1},#{sub2} from courses order by level_id, dont_know DESC"
+    
+    sub1 = "(select sum(courses_users.know) from courses_users where courses_users.course_id = courses.id) know_score"
+    sub2 = "(select levels.name from levels where levels.id = courses.level_id) level_name"
+    @courses = ActiveRecord::Base.connection.execute "select name,#{sub1},#{sub2} from courses order by level_id, know_score"
+    @total_score = User.count() * 2;
+    
     respond_to do |format|
       format.html # index.html.erb
     end
