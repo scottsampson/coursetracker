8e7a7857b4b50afedc3e6917f518b8ccb3eeae77
MattE <matt@mediaclear.net>
2011-02-08 16:56:19 -0500

    Ticket 5- Re-factor the logic for the reports view

--- app/models/course.rb
+++ app/models/course.rb
@@ -7,7 +7,7 @@ class Course < ActiveRecord::Base
   has_many :exercises, :through => :prerequisites
   scope :with_know_score, joins("left JOIN `courses_users` ON `courses_users`.`course_id` = `courses`.`id` INNER JOIN `levels` ON `levels`.`id` = `courses`.`level_id`").group("courses.id").select(["courses.*"," sum(courses_users.know) as know_score, levels.name as level_name"]).order('level_id, know_score')  
 
-  # default_scope order('level_id, order_num')  
+  default_scope order('level_id, order_num')  
   
   #this is used in the reports
   attr_accessor :dont_know 
