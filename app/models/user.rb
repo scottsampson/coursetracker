aedd6ed45c7a65ca5bdbfd66d75570c9dc3c23ef
Scott <scott@cloudspace.com>
2011-03-09 12:56:05 -0500

    Fixing the prerequisites problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,19 +9,19 @@ class User < ActiveRecord::Base
   has_many :answers
   
   def known_courses
-    self.courses.collect{|course| course if course.courses_users.first.know == true }.compact
+    self.courses.collect{|course| course if course.courses_users.first.know > 0 }.compact
   end
   
   def known_beginner
-    self.courses.collect{|course| course if course.courses_users.first.know == true && course.level_id == 1 }.compact
+    self.courses.collect{|course| course if course.courses_users.first.know > 0 && course.level_id == 1 }.compact
   end
   
   def known_intermediate
-    self.courses.collect{|course| course if course.courses_users.first.know == true && course.level_id == 2 }.compact
+    self.courses.collect{|course| course if course.courses_users.first.know > 0 && course.level_id == 2 }.compact
   end
   
   def known_expert
-    self.courses.collect{|course| course if course.courses_users.first.know == true && course.level_id == 3 }.compact
+    self.courses.collect{|course| course if course.courses_users.first.know > 0 && course.level_id == 3 }.compact
   end
   
   def name
